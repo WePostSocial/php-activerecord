9993dab003651f69a51641c949eeccba94272abe
Shane Iseminger <shane@shaneiseminger.com>
2024-05-16 17:13:46 -0600

    No longer can add string + int

--- lib/Column.php
+++ lib/Column.php
@@ -136,11 +136,6 @@ class Column
 		elseif (is_numeric($value) && floor($value) != $value)
 			return (int) $value;
 
-		// If adding 0 to a string causes a float conversion,
-		// we have a number over PHP_INT_MAX
-		elseif (is_string($value) && is_float($value + 0))
-			return (string) $value;
-
 		// If a float was passed and its greater than PHP_INT_MAX
 		// (which could be wrong due to floating point precision)
 		// We'll also check for equal to (>=) in case the precision
